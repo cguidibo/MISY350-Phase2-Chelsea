5f29148e3d1f7307addc80ecbdd8463eab90276d
Chelsea <cguidibo@udel.edu>
2026-05-10 23:05:35 -0400

    Add Owner and Employee subclasses

--- Phase2/service.py
+++ Phase2/service.py
@@ -17,4 +17,10 @@ class User:
         if self.role == "Employee":
             return True
         return False
+class Owner(User):
+    def __init__(self, username, password):
+        super().__init__(username, password, "Owner")
  
+class Employee(User):
+    def __init__(self, username, password):
+        super().__init__(username, password, "Employee")
